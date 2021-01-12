24432a17f997c565056f6e2c04cf15ef83cb1623
Justtuchthat <rutgerwuijster@hotmail.com>
2021-01-12 15:16:42 +0100

    Removed errors from 2 .spec.ts files

--- angular10/src/app/models/movie.spec.ts
+++ angular10/src/app/models/movie.spec.ts
@@ -2,6 +2,6 @@ import { Movie } from './movie';
 
 describe('Movie', () => {
   it('should create an instance', () => {
-    expect(new Movie()).toBeTruthy();
+    expect(new Movie('testActor', 'testCountry', 'testDescription', 'testGenre', 0, 'test/IMBD/url', 'test/img/url', 'testLanguage', 'testScore', 'testRating', 'testTime', 'testTagline', 'testTitle', 'testUserRatingt', 'testVotes', 'testYear', 'testDirector')).toBeTruthy();
   });
 });
